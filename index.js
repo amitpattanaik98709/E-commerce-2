5c86001c0f6592b5841b35d6fbffd960a6f5cb43
amitpattanaik98709 <amitamit98709@gmail.com>
2024-09-26 13:19:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -334,8 +334,8 @@ app.post("/payment", async (req, res) => {
     // payment_methods_types:["card"],
     line_items: lineitems,
     mode: "payment",
-    success_url: "http://localhost:5173/success",
-    cancel_url: "http://localhost:5173/cancel",
+    success_url: "https://e-commerce-2-0zxi.onrender.com/success",
+    cancel_url: "https://e-commerce-2-0zxi.onrender.com/cancel",
   });
 
 
